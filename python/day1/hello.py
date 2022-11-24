6ef0253ecd1f5561a7408498c1107dd7219f6f56
gedevops <webGE@galaxy.com.vn>
2022-11-24 16:24:51 +0700

    create chuoi_trong_python.txt

--- python/day1/hello.py
+++ python/day1/hello.py
@@ -2,3 +2,21 @@ print ("Hello world!", "Created by vult")
 
 print("Toidicode.com", end = " - ")
 print("Hoc lap trinh online")
+
+
+print("Website \"Toidicode.com\" ")
+print('Website \'Toidicode.com\' ')
+
+print("Website \t Toidicode.com")
+
+guy = "Ban"
+full = "Chao mung %s den voi website toidicode.com" %(guy)
+print(full)
+
+
+
+
+guy = "Ban"
+doamin = "toidicode.com"
+full = "Chao mung %s den voi website %s" %(guy, doamin)
+print(full)
