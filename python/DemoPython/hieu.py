8cc8a06209b10d78a5b04f571b5405686f67d386
gedevops <webGE@galaxy.com.vn>
2022-12-13 10:52:24 +0700

    ceate tro_ly_ao

---
+++ python/DemoPython/hieu.py
@@ -0,0 +1,11 @@
+you = "hello"
+
+if you == "":
+    robot_brain= "I can't hear you, try again"
+elif you == "Hello":
+    robot_brain= "Hello Vu Le"
+elif you == "Today":
+    robot_brain= "Thu 3"
+else:
+    robot_brain= "I'm fine thank you and you"
+print (robot_brain)
