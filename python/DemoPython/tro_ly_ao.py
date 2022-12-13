8e22e96aa424596ff3e505904df0d10c3a6d29bf
gedevops <webGE@galaxy.com.vn>
2022-12-13 10:57:12 +0700

    edit tro_ly_ao.py

--- python/DemoPython/tro_ly_ao.py
+++ python/DemoPython/tro_ly_ao.py
@@ -6,29 +6,37 @@ from datetime import date, datetime
 robot_ear = speech_recognition.Recognizer()
 robot_mouth = pyttsx3.init()
 robot_brain = ""
-with speech_recognition.Microphone() as mic:
-    print("Robot: I'm Listening")
-    audio = robot_ear.listen(mic)
-print("Robot: ...")
-try:
-    you = robot_ear.recognize_google(audio)
-except:
-    you = ""
-print ("you: " + you)
+
+while True:
+    with speech_recognition.Microphone() as mic:
+        print("Robot: I'm Listening")
+        audio = robot_ear.listen(mic)
+    print("Robot: ...")
+    try:
+        you = robot_ear.recognize_google(audio)
+    except:
+        you = ""
+    print ("you: " + you)
 
 
-if you == "":
-    robot_brain= "I can't hear you, try again"
-elif "Hello" in you:
-    robot_brain= "Hello Vu Le"
-elif "Today" in you:
-    today = date.today()
-    robot_brain = today.strftime("%B %d, %Y")
-elif "time" in you:
-    now = datetime.now()
-    robot_brain = now.strftime("%H hours %M minutes %S seconds")
-else:
-    robot_brain= "I'm fine thank you and you"
-print ("Robot: " + robot_brain)
-robot_mouth.say(robot_brain)
-robot_mouth.runAndWait()
+    if you == "":
+        robot_brain= "I can't hear you, try again"
+    elif "Hello" in you:
+        robot_brain= "Hello Vu Le"
+    elif "Today" in you:
+        today = date.today()
+        robot_brain = today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        robot_brain = now.strftime("%H hours %M minutes %S seconds")
+    elif "bye" in you:
+        robot_brain = "Bye Vu Le"
+        print ("Robot: " + robot_brain)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
+        break
+    else:
+        robot_brain= "I'm fine thank you and you"
+    print ("Robot: " + robot_brain)
+    robot_mouth.say(robot_brain)
+    robot_mouth.runAndWait()
